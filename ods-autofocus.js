db733b91d11705d17b4d062f54364c9027972435
Kevin Köllmann <kevin.koellmann001@stud.fh-dortmund.de>
2021-08-09 11:33:31 +0200

    Add initial version of ODS Stundennachweis Autofocus Input Field

---
+++ ods-autofocus.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         ODS Stundennachweis Autofocus Input Field
+// @namespace    https://www.kevinkoellmann.de/
+// @version      0.1
+// @description  Sets the focus to the date input field in the "ODS (Online-Dienste für Studierende) Stundennachweis" (engl.~: "online services for students - hourly timesheet")
+// @author       Kevin Köllmann <mail@kevinkoellmann.de>
+// @match        https://ods.fh-dortmund.de/ods*OID=17042419*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let table = document.getElementsByTagName('table')[1];
+
+    let tBody = table.childNodes[1];
+
+    let tableRows = [];
+    tBody.childNodes.forEach(function(item) {
+        if (item.tagName == "TR") tableRows.push(item);
+    });
+
+    let secondToLastRow = tableRows[tableRows.length-2];
+
+    let tableDataCells = [];
+    secondToLastRow.childNodes.forEach(function(item) {
+        if (item.tagName == "TD") tableDataCells.push(item);
+    });
+
+    let firstDataCell = tableDataCells[0];
+    let secondDataCell = tableDataCells[1];
+    let thirdDataCell = tableDataCells[2];
+    let lastDataCell = tableDataCells[tableDataCells.length-1];
+
+    let dateInputField;
+    firstDataCell.childNodes.forEach(function(item) {
+        if (item.tagName == "INPUT") dateInputField = item;
+    });
+
+    let beginTimeInputField;
+    secondDataCell.childNodes.forEach((item) => {
+        if (item.tagName == "INPUT") beginTimeInputField = item;
+    });
+
+    let endTimeInputField;
+    thirdDataCell.childNodes.forEach((item) => {
+        if (item.tagName == "INPUT") endTimeInputField = item;
+    });
+
+    let submitButton;
+    lastDataCell.childNodes.forEach((item) => {
+        if (item.tagName == "INPUT" && item.type == "submit") submitButton = item;
+    });
+
+    dateInputField.tabIndex = 1;
+    beginTimeInputField.tabIndex = 2;
+    endTimeInputField.tabIndex = 3;
+    submitButton.tabIndex = 4;
+
+    dateInputField.focus();
+})();
